4f7bcebba8df3dd0e18801218dcc7751e61f14c1
gab-santi <gabbysantiago96@yahoo.com>
2022-11-16 21:45:17 +0800

    Updated project structure + implemented navbar

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import Navbar from './Navbar'
+import Hero from './Hero'
+import About from './About'
+import Portfolio from './Portfolio'
+import Resume from './Resume'
+import Footer from './Footer'
+
+export {
+    Navbar,
+    Hero,
+    About,
+    Portfolio,
+    Resume,
+    Footer
+}
