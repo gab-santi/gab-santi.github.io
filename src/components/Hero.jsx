ccd22646228123fd86a3e662c4949d0ce1cb655f
gab-santi <gabbysantiago96@yahoo.com>
2022-11-16 22:03:21 +0800

    Finished Hero component main headers

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,26 @@
+import styles from '../styles'
 import React from 'react'
 
-const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+{/* Hero component */}
+const Hero = () => (
+  // main section
+  <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+    {/* subheader section */}
+    <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+      <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+        <p className={`${styles.paragraph} ml-2`}>
+          <span className="text-white">Hello world!</span>
+        </p>
+      </div>
+
+      {/* main header section */}
+      <div className="flex flex-row justify-between items-center w-full">
+        <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white">
+          My name <br /> is <span className="text-gradient">Gab Santiago.</span>
+        </h1>
+      </div>
+    </div>
+  </section>
+)
 
 export default Hero
