794d120c099e3b8cc2fe2803b018435b3684225f
gab-santi <gabbysantiago96@yahoo.com>
2023-01-01 14:28:23 +0800

    Finished Resume page and started Portfolio Page base

--- src/components/PortfolioPage.jsx
+++ src/components/PortfolioPage.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import styles from "../styles";
+
+import Footer from "./Footer";
 
 const PortfolioPage = () => {
-  return <div>PortfolioPage</div>;
+  return (
+    <section id="portfoliopage">
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          {/* Portfolio Page label */}
+          <h3 className="font-poppins font-semibold ss:text-[50px] text-[50px] text-black ss:leading-[100px] leading-[75px] w-full">
+            Portfolio
+          </h3>
+
+          <Footer />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default PortfolioPage;
--- src/components/ResumePage.jsx
+++ src/components/ResumePage.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import styles from "../styles";
+
+import Footer from "./Footer";
 
 const ResumePage = () => {
-  return <div>ResumePage</div>;
+  return (
+    <section id="portfoliopage">
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          {/* Portfolio Page label */}
+          <h3 className="font-poppins font-semibold ss:text-[50px] text-[50px] text-black ss:leading-[100px] leading-[75px] w-full">
+            Resume
+          </h3>
+
+          <a
+            href="https://drive.google.com/file/d/1cy8LLSZhwfpuZHfOA_TSa4rJ4vfCHjSe/view?usp=sharing"
+            target="_blank"
+            className="font-poppins underline"
+          >
+            ⬇️ Download
+          </a>
+
+          {/* Resume embedded from GDrive */}
+          <div className="flex flex-col items-center">
+            <iframe
+              src="https://drive.google.com/file/d/1cy8LLSZhwfpuZHfOA_TSa4rJ4vfCHjSe/preview"
+              height="480"
+              className="mt-5 sm:w-[100%]"
+              allow="autoplay"
+            ></iframe>
+          </div>
+          <Footer />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default ResumePage;
